e220e89c6fb1daf5bf66f4f14813cde15d6c8773
danielferreiradf <danielferreiradk@gmail.com>
2018-11-08 12:21:05 -0200

    Updates

---
+++ scripts/scripts.js
@@ -0,0 +1,24 @@
+// Carousel
+
+$(document).ready(function () {
+    $('.showcase__carousel').slick({
+        arrows: true,
+        infinite: true,
+        speed: 400,
+        fade: true,
+        cssEase: 'linear',
+    });
+});
+
+// // Hover album 
+// const albums = document.querySelectorAll('.albums');
+// albums.forEach((album) => {
+//     album.addEventListener('mouseover', () => {
+//        album.drawImage 
+//     });
+// });
+// const imgHover = document.createElement('img');
+// imgHover.src = '../imgs/'
+
+
+
